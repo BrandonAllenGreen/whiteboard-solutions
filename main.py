8df608b0004c1c0a55b2c646555004c7ffef0f2a
BrandonAllenGreen <green04@gmail.com>
2018-08-09 15:38:24 -0500

    pushing latest

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-'''///////////// JavaScript Whiteboard Problems & Solutions /////////////'''
+'''///////////// Python Whiteboard Problems & Solutions /////////////'''
 
 
 ''' Write a program that console logs the numbers from 1 to n. But for multiples of three print “fizz”
@@ -14,6 +14,15 @@ for num in range(1, 101):
         print('Buzz')
     else:
         print(num)
+
+
+''' Write a function that takes a string and determines whether it's a palindrome or not. 
+Ex:  "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!" ➞ True '''
+
+
+def is_palindrome(txt):
+    check = [s.lower() for s in txt if s.isalpha()]
+    return check[::-1] == check
 
 
 ''' Create a function that takes a list of strings. Return all words in the list that are exactly four letters.
@@ -42,3 +51,31 @@ def minMaxLengthAverage(arr):
     arrLen = len(arr)
     arrAvg = sum(arr)/arrLen
     return [arrMin, arrMax, arrLen, arrAvg]
+
+
+''' Create a function that accepts a string as an argument. Find its shortest word(s) and return them as a list sorted alphabetically (if there are multiple shortest words). 
+Ex: "The quick brown fox jumped over the lazy dogs back." ➞ ['fox', 'the', 'the'] '''
+
+
+def findShortestWords(txt):
+    all_len = set()
+    for i in txt[:-1].split():
+        all_len.add(len(i))
+    return sorted([i.lower() for i in txt[:-1].split() if len(i) == min(all_len) and i.isalpha()])
+
+
+''' You have a list of integers, and for each index you want to find the product of every integer except the integer at that index.  Write a function that takes a list of integers and returns a list of the products.
+Ex: [1, 7, 3, 4] ➞ [84, 12, 28, 21] '''
+
+
+def get_products(lst):
+    prod_list = []
+
+    for i in lst:
+        prod = 1
+        mylst = [x for x in lst if x != i]
+        for j in mylst:
+            prod *= j
+        prod_list.append(prod)
+
+    return prod_list
